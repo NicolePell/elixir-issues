f940d8f009086f278659374bc13e021a7e66ee8e
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-11-16 23:31:06 +0000

    Add library to parse JSON.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -12,11 +12,24 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
-    { :ok, body }
+    { :ok, Poison.Parser.parse!(body) }
   end
 
   def handle_response({ _, %{status_code: _, body: body}}) do
-    { :error, body }
+    { :error, Poison.Parser.parse!(body) }
+  end
+
+  def process({ user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response(:error, error) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
   end
 
 end
